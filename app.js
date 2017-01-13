35a8894b205271e0d83ea2bf4ce787019b80f466
guillermo.fernanez <guillermo.fernandez@uruit.com>
2017-01-13 18:52:47 -0300

    Fix state provider

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ angular.module('app', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     $stateProvider
+        .state('main',{
+            url: '/main',
+            template: '<h1>Welcome to angular-ui-router demo.'
+        })
         .state('home', {
+            url: '/home',
             views:
                 {
-                    'header': { templateUrl: 'header.html' },
-                    'content': { templateUrl: 'content.html' },
-                    'footer': { templateUrl: 'footer.html' },
+                    'header': {
+                        templateUrl: '/header.html'
+                    },
+                    'content': {
+                        templateUrl: '/content.html'
+                    },
+                    'footer': {
+                        templateUrl: '/footer.html'
+                    },
                 }
         })
 
